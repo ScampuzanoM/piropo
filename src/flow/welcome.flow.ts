e66a2378e3457ae984b927eefd3bdc9a4d64542f
ScampuzanoM <sebascm3450@gmail.com>
2024-06-19 08:47:29 -0500

    feat: :zap: mensajes

--- src/flow/welcome.flow.ts
+++ src/flow/welcome.flow.ts
@@ -7,28 +7,27 @@ import { reset, start } from '../idle-custom'
 
 
 export const welcomeFlow = addKeyword<Provider, Database>(['hola', 'hoola', 'ole', 'alo', 'buenas', 'menu', 'holi', 'hol', 'oe', 'buenos'])
-    .addAnswer(
-        [
-            `Llegaste a perriar a
-            LOCAL DE PERRAS! 🌭
-
-            ¿De qué estás antojado hoy?
-            Todos nuestros productos son artesanales e irresistibles. Nuestro producto estrella es ⭐️ la Perra especial grande.
-
-            🤤 Antójate haciendo click aquí: [enlace a story en redes actualizada] - pendiente valen @~Vale
-
-            😎 Abrimos todos los días a las 5pm
-            📍 Sede Sabaneta y Envigado
-
-            QUÉ QUIERES HOY?
-
-            La experiencia para perriar con nosotros es muy fácil. Solo debes responder con el número según lo que necesites:
-
-            1. 🛵 Domicilio
-            2. 🥡 Pedir y recoger en tienda
-            3. 🗣️ Hablar con alguien
-            `
-        ].join('\n'),
+.addAnswer(
+    [
+        'Llegaste a perriar a',
+        'LOCAL DE PERRAS! 🌭',
+        '',
+        '¿De qué estás antojado hoy?',
+        'Todos nuestros productos son artesanales e irresistibles. Nuestro producto estrella es ⭐️ la Perra especial grande.',
+        '',
+        '🤤 Antójate haciendo click aquí: [enlace a story en redes actualizada] - pendiente valen @~Vale',
+        '',
+        '😎 Abrimos todos los días a las 5pm',
+        '📍 Sede Sabaneta y Envigado',
+        '',
+        'QUÉ QUIERES HOY?',
+        '',
+        'La experiencia para perriar con nosotros es muy fácil. Solo debes responder con el número según lo que necesites:',
+        '',
+        '1. 🛵 Domicilio',
+        '2. 🥡 Pedir y recoger en tienda',
+        '3. 🗣️ Hablar con alguien'
+    ].join('\n'),
         {capture: true},
         async (ctx, { gotoFlow, fallBack}) => {
                 reset(ctx, gotoFlow, Number(process.env.TIEMPOINACTIVIDAD));
